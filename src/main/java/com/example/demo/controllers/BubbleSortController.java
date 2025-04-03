743860b8651e9f81b657c485b6c4c274f4cc99e6
Valentin Almeida <valentinfalmeida@hotmail.com>
2025-04-02 22:47:23 -0300

    fix(bubble-sort): retirado metodo http incorreto

--- src/main/java/com/example/demo/controllers/BubbleSortController.java
+++ src/main/java/com/example/demo/controllers/BubbleSortController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/sort")
 public class BubbleSortController {
 
-    @PostMapping("/bubble")
+    @GetMapping("/bubble")
     public String sortArray(@Valid @RequestBody BubbleSortRequest request) {
         return BubbleSortSerializer.serialize(BubbleSortCalculator.create(request.toDTO()));
     }
